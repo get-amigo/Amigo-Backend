29f4d090ae62f4176a59b90d3aa7119921d59583
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-12-26 21:09:41 +0530

    [ADD] twilio message to inivite user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import UserSchema from './users.schema';
-
+import Twilio from 'twilio';
 @Injectable()
 export class UsersService {
   constructor(
@@ -27,21 +27,36 @@ export class UsersService {
   
       // If the user doesn't exist, create a new one
       if (!user) {
+        const {phoneNumber,countryCode}=phoneNumbers[i];
         const newUser = new this.userModel({
-          phoneNumber: phoneNumbers[i].phoneNumber,
-          countryCode: phoneNumbers[i].countryCode
+          phoneNumber,
+          countryCode
         });
   
         user = await newUser.save();
+        this.sendSMS(phoneNumbers,countryCode,"Join our app");
       }
   
-      // Add the user's ID to the list
       userIds.push(user._id);
     }
   
     return userIds;
   }
   
+  async sendSMS(phoneNumber,countryCode,message)
+  {
+    if(process.env.ENV=="production"){
+      const client = Twilio(
+        process.env.TWILIO_ACCOUNT_SID,
+        process.env.TWILIO_AUTH_TOKEN,
+      );
+      client.messages.create({
+        body: message,  // Text of the SMS
+        to: '+'+countryCode+phoneNumber,
+        from: process.env.TWILIO_PHONE_NUMBER          // Replace with a valid Twilio number
+    });
+  }
+}
 
   async findById(id) {
     return await this.userModel.findById(id).exec();
